8765cf30fa881961c061a1830f9ee01e87972b51
bambketan <bambketan@gmail.com>
2021-12-11 15:02:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-oksods
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-oksods)
---
+++ web-srver/src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+//const path = require('path');
+console.log('OKK');
+console.log('dds');
+app.get('', (req, res) => {
+  res.send('<h1>Used data</h1>');
+});
+
+app.get('/help', (req, res) => {
+  res.send('Oh HElp');
+});
+
+app.get('/about', (req, res) => {
+  res.send('about');
+});
+
+app.get('/weather', (req, res) => {
+  res.send({ name: 'KEtan', shop: 'Sagar' });
+});
+app.listen(3000, () => {
+  console.log('listing on port 3000');
+});
